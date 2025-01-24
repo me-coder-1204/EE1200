3e40198658ee97067746ea2b622973ee5738e8c2
me-coder-1204 <69784401+me-coder-1204@users.noreply.github.com>
2025-01-24 10:47:07 +0530

    add figs-act1

--- Activity1/code/code.py
+++ Activity1/code/code.py
@@ -3,17 +3,19 @@ import numpy as np
 
 t = np.linspace(0, 10*np.pi,10000)
 
-f1 = 4
-f2 = 12
+f1 = 5
+f2 = 6
 A = 5
 B = 5
-ph = 90*np.pi/180
+deg = 0
+ph = deg*np.pi/180
 
 x = A*np.sin((2*np.pi/f1)*t + ph)
 y = B*(np.sin((2*np.pi/f2)*t))
 
 plt.grid()
 plt.axis("equal")
-plt.scatter(y,x)
-plt.savefig("../figs/fig4.png")
+plt.scatter(y,x,label = f"$f_1 = {f1}, f_2 = {f2}, \\phi = {deg}^\\circ$")
+plt.legend(loc="best")
+plt.savefig("../figs/fig8.png")
 plt.show()
