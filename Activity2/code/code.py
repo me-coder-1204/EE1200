388470640d39c263b716f9580888f06f696cda2e
me-coder-1204 <69784401+me-coder-1204@users.noreply.github.com>
2025-01-28 18:37:56 +0530

    add experiment 2

--- Activity2/code/code.py
+++ Activity2/code/code.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 from scipy import signal
 
 n = 10**3
-h = 1/10**10
-p = 10
+h = 1/10**7
+p = 50
 
 
 T = n*h
 
 t = np.linspace(0,p*T,p*n)
 
-T = 2
+# T = 2
 
 R = 1000
-C = 1/10**6
-Ce = 10**-3
+C = 1/10**7
+Ce = 0*10**-3
 V0 = 5
 
 
@@ -47,5 +47,14 @@ f = 5*np.exp(-t/(R*C))
 # plt.plot(t, Vr, label='r')
 plt.plot(t,Vc, color='r')
 # plt.plot(t,f,label='l')
+
+Vl = -V0*((1-np.exp(-T/(2*R*C)))/(1+np.exp(-T/(2*R*C))))
+Vu = -Vl
+a = np.array([[0, Vu],[p*T, Vu]])
+
+plt.plot(a[:,0],a[:,1], label = 'k')
+
+print(T, R*C, Vu)
 plt.grid()
+plt.savefig("./fig5.png")
 plt.show()
